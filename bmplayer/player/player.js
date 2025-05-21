138fa7eeb58224f7756f616af5c87feaa6ed672b
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-05-21 15:23:10 -0300

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -11,7 +11,7 @@ function setLink() {
 		e = e[1];
 		alert(e);
 
-var streams = [e];
+var streams = ["https://tv03.zas.media:1936/rftv/rftv/playlist.m3u8","/error.m3u8"]
 	}
 function isIOS() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
 var script = document.createElement('script');
@@ -29,11 +29,11 @@ document.head.appendChild(script);
 		//}
 	
 
-	if(window.bradmax && window.bradmax.player) {
-		setLink();
-	} else {
-		window.addEventListener('load', setLink);
-		}
+	//if(window.bradmax && window.bradmax.player) {
+		//setLink();
+	//} else {
+		//window.addEventListener('load', setLink);
+		//}
 
 
 
