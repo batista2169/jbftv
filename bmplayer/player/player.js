f610e9c2a08566ebee260b690d94ad57cea2d18b
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-05-21 14:51:45 -0300

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -11,17 +11,17 @@ function setLink() {
 		e = e[1];
 		alert(e);
 
-var streams = ['e','/error.m3u8'];
-function isIOS() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
-var script = document.createElement('script');
-script.src = isIOS() ? 'https://televisao.tv/js/ios.js' : 'https://televisao.tv/js/hls.js';
-document.head.appendChild(script);
+//var streams = [e];
+//function isIOS() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
+//var script = document.createElement('script');
+//script.src = isIOS() ? 'https://televisao.tv/js/ios.js' : 'https://televisao.tv/js/hls.js';
+//document.head.appendChild(script);
 //video.load();
 //video.play();
 	
-	//var bradmaxPlayerConfig = {dataProvider:{source:[{url:e}]},autoplay:true};
-	//var element = document.getElementById("jb_player");
-	//var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
+	var bradmaxPlayerConfig = {dataProvider:{source:[{url:e}]},autoplay:true};
+	var element = document.getElementById("script");
+	var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
 
 	if(!window.player) {
 		window.player = player;
