71ff04e1b7772cfe11ad8025a37a47541b0121c3
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-05-22 09:58:44 -0300

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -17,25 +17,89 @@ var settingsMenu = document.createElement('settingsMenu');
 settingsMenu.src = isIOS() ? 'https://televisao.tv/js/ios.js' : 'https://televisao.tv/js/hls.js';
 document.head.appendChild(script);
 
-//video.src=e;
-//video.load();
-//video.play();
+var video = document.getElementById('settingsMenu');
+
+function playM3u8(url){
+  if(Hls.isSupported()) {
+      video.volume = 0.3;
+      var hls = new Hls();
+      var m3u8Url = decodeURIComponent(url)
+      hls.loadSource(m3u8Url);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED,function() {
+        video.play();
+      });
+      document.title = url
+    }
+	else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+		video.src = url;
+		video.addEventListener('canplay',function() {
+		  video.play();
+		});
+		video.volume = 0.3;
+		document.title = url;
+  	}
+}
+
+function playPause() {
+    video.paused?video.play():video.pause();
+}
+
+function volumeUp() {
+    if(video.volume <= 0.9) video.volume+=0.1;
+}
+
+function volumeDown() {
+    if(video.volume >= 0.1) video.volume-=0.1;
+}
+
+function seekRight() {
+    video.currentTime+=5;
+}
+
+function seekLeft() {
+    video.currentTime-=5;
+}
+
+function vidFullscreen() {
+    if (video.requestFullscreen) {
+      video.requestFullscreen();
+  } else if (video.mozRequestFullScreen) {
+      video.mozRequestFullScreen();
+  } else if (video.webkitRequestFullscreen) {
+      video.webkitRequestFullscreen();
+    }
+}
+
+playM3u8(window.location.href.split("?source=")[1])
+$(window).on('load', function () {
+    $('#video').on('click', function(){this.paused?this.play():this.pause();});
+    Mousetrap.bind('space', playPause);
+    Mousetrap.bind('up', volumeUp);
+    Mousetrap.bind('down', volumeDown);
+    Mousetrap.bind('right', seekRight);
+    Mousetrap.bind('left', seekLeft);
+    Mousetrap.bind('f', vidFullscreen);
+});
+
+
+
 
 	
 	//var bradmaxPlayerConfig = {dataProvider:{source:[{url:e}]},autoplay:true};
 	//var element = document.getElementById("settingsMenu");
 	//var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
 
-	if(!window.player) {
-		window.player = player;
-		}
+	//if(!window.player) {
+		//window.player = player;
+		//}
 	
 
-	if(window.player && window.bradmax.player) {
-		setLink();
-	} else {
-		window.addEventListener('load', setLink);
-		}
+	//if(window.player && window.bradmax.player) {
+		//setLink();
+	//} else {
+		//window.addEventListener('load', setLink);
+		//}
 
 
 
