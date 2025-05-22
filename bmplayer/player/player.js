2846f165c412f16034446d925c754915c4a6df2b
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-05-22 15:13:45 -0300

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -10,7 +10,7 @@ function setLink() {
 		e = e.split('?source=');
 		e = e[1];
 		alert(e);
-	var url = '["'+ e +'","/error.m3u8"]'
+	var url = '["'+ e +'","/error.m3u8"];'
 	alert(url);
 }
 var streams = '["'+ e + ","/error.m3u8"];
