7a02de86a8667a7bd90fcb36c26e6e44ee4cc9f8
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-05-23 09:21:55 -0300

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -3,7 +3,13 @@
     SPDX-License-Identifier: MIT
     JBF-TV Player - R.Santana/BA
 */
-
+function setLink() {	
+	var settingsMenu = document.getElementById("settingsMenu");
+	var n = window.location.href;
+		n = n.split('?source=');
+		n = n[1];
+		alert(n);
+}
 
 	//var url = '["'+ e +'","/error.m3u8"]';
 	//url = url[1];
@@ -17,11 +23,11 @@
 //var streams = oldstreams.replace(/teste.m3u8/g, e);
 //var streams = ["teste.m3u8","/error.m3u8"];
 //console.log(streams);
-var streams = ["https://tv03.zas.media:1936/rftv/rftv/playlist.m3u8","/error.m3u8"];
-function isIOS() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
-var script = document.createElement('script');
-script.src = isIOS() ? 'https://televisao.tv/js/ios.js' : 'https://televisao.tv/js/hls.js';
-document.head.appendChild(script);
+//var streams = ["https://tv03.zas.media:1936/rftv/rftv/playlist.m3u8","/error.m3u8"];
+//function isIOS() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
+//var script = document.createElement('script');
+//script.src = isIOS() ? 'https://televisao.tv/js/ios.js' : 'https://televisao.tv/js/hls.js';
+//document.head.appendChild(script);
 
 //console.log(streams);
 //function changeSource(url) {
@@ -32,22 +38,16 @@ document.head.appendChild(script);
 	//var element = document.getElementById("settingsMenu");
 	//var player = window.bradmax.player.create(element, bradmaxPlayerConfig);
 
-	//if(!window.player) {
-		//window.player = player;
-		//}
+	if(!window.player) {
+		window.player = player;
+		}
 	
 
-	//if(window.player && window.bradmax.player) {
-		//setLink();
-	//} else {
-		//window.addEventListener('load', setLink);
-		//}
+	if(window.player && window.bradmax.player) {
+		setLink();
+	} else {
+		window.addEventListener('load', setLink);
+		}
 
-function setLink() {	
-	var settingsMenu = document.getElementById("settingsMenu");
-	var n = window.location.href;
-		n = n.split('?source=');
-		n = n[1];
-		alert(n);
-}
+
 
