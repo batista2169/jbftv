19914be514495ee6139388c3da43c51df0faba8c
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-05-23 08:26:02 -0300

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -24,10 +24,10 @@ function isIOS() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window
 var script = document.createElement('script');
 script.src = isIOS() ? 'https://televisao.tv/js/ios.js' : 'https://televisao.tv/js/hls.js';
 document.head.appendChild(script);
-var oldstreams = "https://tv03.zas.media:1936/rftv/rftv/playlist.m3u8";
-var streams = oldstreams.replace(/https://tv03.zas.media:1936/rftv/rftv/playlist.m3u8/g, e);
-var streams = ["https://tv03.zas.media:1936/rftv/rftv/playlist.m3u8","/error.m3u8"];
-
+var oldstreams = "teste.m3u8";
+var streams = oldstreams.replace(/teste.m3u8/g, e);
+var streams = ["teste.m3u8","/error.m3u8"];
+console.log(streams);
 //function changeSource(url) {
    
 //}
