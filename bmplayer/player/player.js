df5b2c064cbdd212c31441c83e2f1623b40b6739
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-05-26 12:53:21 +0000

    Update player.js

--- bmplayer/player/player.js
+++ bmplayer/player/player.js
@@ -21,7 +21,7 @@ var t = window.location.href;
 
 var script = document.createElement('script');
 script.src = setLink() ? 'https://televisao.tv/js/ios.js' : 'https://televisao.tv/js/hls.js';
-	var url = '["'+ t +'","/error.m3u8"];';
+	var url = '["'+ t +'","/error.m3u8"]';
 var streams = url;
 alert(url);
 document.head.appendChild(script);
