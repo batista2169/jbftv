71df862525ab7ae13e85ac3ef5cd2376997fc62b
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-05-27 14:21:06 -0300

    Update player.js

--- vdplayer/player/js/player.js
+++ vdplayer/player/js/player.js
@@ -6,15 +6,14 @@
 
 function setLink() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
 var url = window.location.href;
-	url = url.split('?source=');
-	url = url[1];
-	//alert(url);
+    url = url.split('?source=');
+    url = url[1];
+    //alert(url);
 var e = '/error.m3u8';
-	//alert(e);
 var script = document.createElement('script');
 script.src = setLink() ? 'js/ios.js' : 'js/hls.js';
 document.head.appendChild(script);
-//var streams = [url,"/error.m3u8"];
 var streams = [url,e];
+//var streams = [url,"/error.m3u8"];
 	
 
