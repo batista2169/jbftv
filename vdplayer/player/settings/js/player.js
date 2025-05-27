f921b044203a830eb7685aae52e5ad631bd6ebaa
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-05-27 15:00:41 -0300

    Add files via upload

---
+++ vdplayer/player/settings/js/player.js
@@ -0,0 +1,19 @@
+/*
+    Copyright (c) 2024 Pierleeb
+    SPDX-License-Identifier: MIT
+    JBF-TV Player - R.Santana/BA
+*/
+
+function setLink() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
+var url = window.location.href;
+    url = url.split('?source=');
+    url = url[1];
+    //alert(url);
+var e = '/error.m3u8';
+var script = document.createElement('script');
+script.src = setLink() ? 'js/ios.js' : 'js/hls.js';
+document.head.appendChild(script);
+var streams = [url,e];
+//var streams = [url,"/error.m3u8"];
+	
+
