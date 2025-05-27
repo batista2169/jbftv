cc8f29d737dd2428f9b0ddd08b42f813dffd157b
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-05-27 12:05:25 -0300

    Update player.js

--- vdplayer/player/player.js
+++ vdplayer/player/player.js
@@ -4,18 +4,18 @@
     JBF-TV Player - R.Santana/BA
 */
 
-function setLink() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
-var url = window.location.href;
-	url = url.split('?source=');
-	url = url[1];
+//function setLink() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
+//var url = window.location.href;
+	//url = url.split('?source=');
+	//url = url[1];
 	//alert(url);
-var e = '/error.m3u8';
+//var e = '/error.m3u8';
 	//alert(e);
 
-var script = document.createElement('script');
-script.src = setLink() ? 'js/ios.js' : 'js/hls.js';
-document.head.appendChild(script);
+//var script = document.createElement('script');
+//script.src = setLink() ? 'js/ios.js' : 'js/hls.js';
+//document.head.appendChild(script);
 //var streams = [url,"/error.m3u8"];
-var streams = [url,e];
+//var streams = [url,e];
 	
 
