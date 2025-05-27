3d73978c048de94d99d4169399133efdf3945c28
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-05-27 11:11:28 -0300

    Add files via upload

---
+++ vdplayer/main.js
@@ -0,0 +1,22 @@
+/*
+    Copyright (c) 2024 Pierleeb
+    SPDX-License-Identifier: MIT
+    JBF-TV Player - R.Santana/BA
+*/
+
+function setLink(){
+	var input = document.form.input;
+	var inputText = input.value;
+	if (input.value == ""){
+		window.alert("Url do vídeo ou áudio é obrigatório!");
+		input.focus();
+        	return false;
+	}else {
+	let settingsMenu = document.getElementById("settingsMenu");
+	let input = document.getElementById("input");
+	let inputText = input.value;
+		window.alert('Canal ' + inputText + ' inserido com sucesso!');
+		window.location.href = './player' + '?source=' + inputText;	   
+		}
+		input.value = ''
+	}
