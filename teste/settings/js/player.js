ee281cb712f9d1cc4239ab27a23fa4c85b840c4b
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-06-06 07:39:42 -0300

    Add files via upload

---
+++ teste/settings/js/player.js
@@ -0,0 +1,37 @@
+/*
+    Copyright (c) 2024 Pierleeb
+    SPDX-License-Identifier: MIT
+    JBF-TV Player - R.Santana/BA
+*/
+function Carregar(){
+	document.getElementById("input").value = localStorage.getItem("input");
+
+	}
+
+function setLink(){
+var input = document.form.input;
+var inputText = input.value;
+	if (input.value == ""){
+		window.alert("Url do vídeo ou áudio é obrigatório!");
+		input.focus();
+		return false;
+	}else {
+
+		window.alert('Canal ' + inputText + ' inserido com sucesso!');
+		localStorage.setItem("input",document.getElementById("input").value);
+		location.reload(); 
+
+		}
+}
+
+function Link() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
+//var t = '/error.m3u8';
+var script = document.createElement('script');
+script.src = Link() ? 'settings/js/ios.js' : 'settings/js/hls.js';
+document.head.appendChild(script);
+//var streams = [url];
+var video = document.getElementById("video");
+var input = document.getElementById("input");
+var inputText = input.value;
+var e = input.value;
+var streams = [e,"/error.m3u8"];
