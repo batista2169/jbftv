4cd15a3ef89ca93ea0f11bc5d09ddec3b122297f
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-06-09 14:50:23 -0300

    Update player.js

--- teste/settings/js/player.js
+++ teste/settings/js/player.js
@@ -21,13 +21,13 @@ var inputText = input.value;
 		//location.reload(); 
 
 		}
-}
-
-function Link() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 	var video = document.getElementById("video");
 var input = document.getElementById("input");
 var inputText = input.value;
-var e = input.value;	}
+var e = input.value;
+}
+
+function Link() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
 //var t = '/error.m3u8';
 var script = document.createElement('script');
 script.src = Link() ? 'settings/js/ios.js' : 'settings/js/hls.js';
