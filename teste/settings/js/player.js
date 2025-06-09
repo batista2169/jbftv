fd5375d8f408b846f9d31a8168f2ddbb431990b0
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-06-09 14:34:54 -0300

    Update player.js

--- teste/settings/js/player.js
+++ teste/settings/js/player.js
@@ -26,13 +26,14 @@ var inputText = input.value;
 function Link() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
 //var t = '/error.m3u8';
 var script = document.createElement('script');
-script.src = Link() ? 'settings/js/ios.js' : 'settings/js/hls.js';
-document.head.appendChild(script);
-//var streams = [url];
 var video = document.getElementById("video");
 var input = document.getElementById("input");
 var inputText = input.value;
 var e = input.value;
+script.src = Link() ? 'settings/js/ios.js' : 'settings/js/hls.js';
+document.head.appendChild(script);
+//var streams = [url];
+
 var streams = [e,"/error.m3u8"];
 
 
