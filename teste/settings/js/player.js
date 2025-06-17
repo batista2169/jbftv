e035497dfe8a0c13052436972bff53de3c814a02
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-06-17 10:28:27 -0300

    Update player.js

--- teste/settings/js/player.js
+++ teste/settings/js/player.js
@@ -3,9 +3,6 @@
     SPDX-License-Identifier: MIT
     JBF-TV Player - R.Santana/BA
 */
-function Carregar(){
-document.getElementById("input").value = localStorage.getItem("input");	
-	}
 
 function setLink(){
 var input = document.form.input;
@@ -16,19 +13,55 @@ var inputText = input.value;
 		return false;
 	}else {
 		window.alert('Canal ' + inputText + ' inserido com sucesso!');
-  		localStorage.setItem("input",document.getElementById("input").value);		
-		window.location.href = '?source=' + input.value; 
-		}
-	}
-function Link() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
+		//localStorage.setItem("input",document.getElementById("input").value);		
 
-var url = window.location.href;
-    url = url.split('?source=');
-    url = url[1];
-    alert(url);
+var jb_player = document.getElementById("jb_player");
+var input = document.querySelector("#input").value;
+	localStorage.setItem("arq", input);
+	//alert("O valor guardado e: " + localStorage.getItem("arq"))
+//var url = window.localStorage.getItem('arq'); 
+
+window.location.reload();
+	}	
+}
+
+function Link() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
+var url = window.localStorage.getItem('arq'); 
+//window.alert(url);
 var e = '/error.m3u8';
 var script = document.createElement('script');
 script.src = Link() ? 'settings/js/ios.js' : 'settings/js/hls.js';
 document.head.appendChild(script);
 var streams = [url,e];
+//var streams = [url,"/error.m3u8"];
+localStorage.clear();
+
+
+//function Carregar(){
+//document.getElementById("input").value = localStorage.getItem("input");	
+	//}
+
+//function setLink(){
+//var input = document.form.input;
+//var inputText = input.value;
+	//if (input.value == ""){
+		//window.alert("Url do vídeo ou áudio é obrigatório!");
+		//input.focus();
+		//return false;
+	//}else {
+		//window.alert('Canal ' + inputText + ' inserido com sucesso!');
+  		//localStorage.setItem("input",document.getElementById("input").value);		
+		//window.location.href = '?source=' + input.value; 
+		//}
+	//}
+//function Link() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
+//var url = window.location.href;
+    //url = url.split('?source=');
+    //url = url[1];
+    //alert(url);
+//var e = '/error.m3u8';
+//var script = document.createElement('script');
+//script.src = Link() ? 'settings/js/ios.js' : 'settings/js/hls.js';
+//document.head.appendChild(script);
+//var streams = [url,e];
 //var streams = [url,"/error.m3u8"];
