91b57f836b543d7eb35628225f977eae05237976
batista2169 <71348110+batista2169@users.noreply.github.com>
2025-06-17 08:21:54 -0300

    Update player.js

--- teste/settings/js/player.js
+++ teste/settings/js/player.js
@@ -3,61 +3,32 @@
     SPDX-License-Identifier: MIT
     JBF-TV Player - R.Santana/BA
 */
-//function Carregar(){
-	//document.getElementById("input").value = localStorage.getItem("input");
-	//}
+function Carregar(){
+document.getElementById("input").value = localStorage.getItem("input");	
+	}
+
 function setLink(){
-//var input = document.form.input;
-//var inputText = input.value;
-	//if (input.value == ""){
-		//window.alert("Url do vídeo ou áudio é obrigatório!");
-		//input.focus();
-		//return false;
-	//}else {
+var input = document.form.input;
+var inputText = input.value;
+	if (input.value == ""){
+		window.alert("Url do vídeo ou áudio é obrigatório!");
+		input.focus();
+		return false;
+	}else {
+		window.alert('Canal ' + inputText + ' inserido com sucesso!');
+  		localStorage.setItem("input",document.getElementById("input").value);		
+		window.location.href = '?source=' + input.value; 
+		}
+	}
+function Link() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
 
-		//window.alert('Canal ' + inputText + ' inserido com sucesso!');
-		//localStorage.setItem("input",document.getElementById("input").value);
-  const input = document.querySelector("#input").value;
-  localStorage.setItem("link", input);
-  console.log(input);
-		alert("O valor guardado e: " + localStorage.getItem("link"))
-
-		var u = window.localStorage.getItem('link'); 
-
-
-		//window.location.reload();
-		//setTimeout(function (setLin) {
-       // location.reload()
-    //}, 1000); .split(",")
-
-
-//alert(u);
-		location.reload()
-localStorage.setItem("input",document.getElementById("input").value);
-//video.src=u
-//video.load()
-//video.play()
-
-
-		
-		//}
-}
-const input = document.querySelector("#input").value;
-  localStorage.setItem("link", input);
-  console.log(input);
-  alert("O valor guardado e: " + localStorage.getItem("link"))
-  var u = window.localStorage.getItem('link');
-var streams = [u];
-function setLin() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
-//var e = '/error.m3u8';
+var url = window.location.href;
+    url = url.split('?source=');
+    url = url[1];
+    alert(url);
+var e = '/error.m3u8';
 var script = document.createElement('script');
-script.src = setLin() ? 'settings/js/ios.js' : 'settings/js/hls.js';
+script.src = Link() ? 'settings/js/ios.js' : 'settings/js/hls.js';
 document.head.appendChild(script);
+var streams = [url,e];
 //var streams = [url,"/error.m3u8"];
-
-
-//function play() {    
-   // setLin(function () {	   
-	//    alert("O valor guardado e: " + localStorage.getItem("link"))
-   // }, 100);
-//}
